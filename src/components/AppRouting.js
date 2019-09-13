0d857eb7a1d14758fc8c8108554f287a71ef1282
Cobrettie <cobrettiegarner3@gmail.com>
2019-09-13 19:53:15 -0400

    map over locations, create locatio card, render successfuln

--- src/components/AppRouting.js
+++ src/components/AppRouting.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route } from "react-router-dom";
 import WelcomePage from './WelcomePage';
 import CharacterList from './CharacterList';
+import LocationsList from './LocationsList';
 
 const AppRouting = () => {
     return (
         <div>
             <Route exact path="/" component={WelcomePage} />
             <Route path="/characters" component={CharacterList} />
+            <Route path="/locations" component={LocationsList} />
         </div>
     )
 }
--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,3 +1,26 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import LocationCard from './LocationCard';
 
-export default function LocationsList() {}
+export default function LocationsList() {
+    // add useState
+    const [locations, setLocations] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        setIsLoading(true)
+        axios
+        .get("https://rickandmortyapi.com/api/location/")
+        .then(res => {
+            setLocations(res.data.results)
+            setIsLoading(false)
+        })
+        .catch(err => console.error(err))
+    }, [])
+
+    return (
+        <section className="grid-view">
+            {!isLoading ? locations.map(location => <LocationCard location={location} />) : (<p>Loading...</p>)}
+        </section>
+    )
+}
